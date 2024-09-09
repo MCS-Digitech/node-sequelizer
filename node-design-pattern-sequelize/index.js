f9855eee638433461e04658713265410a3b94e86
MCS-Digitech <digitechmcs@gmail.com>
2024-09-09 14:16:12 -0700

    Add files via upload

---
+++ node-design-pattern-sequelize/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const db = require("./src/database/db");
+const app = express();
+
+let context = {
+  db: undefined,
+  redis: undefined,
+  validation: undefined,
+};
+
+app.listen(8080, async () => {
+  try {
+    console.log("app starting");
+    context.db = await db();
+    console.log("server is listening on 3000");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get("/", (req, res) => {
+  res.status(200).send("ok server is running");
+});
